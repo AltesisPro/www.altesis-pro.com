7f7488761dae9550013c1f2241dbbddb8a93b18b
marcosantonastasi <marco.santonastasi@gmail.com>
2022-09-14 09:59:37 +0200

    draft v0.1

---
+++ _config.ts
@@ -0,0 +1,21 @@
+import lume from "lume/mod.ts";
+import base_path from "lume/plugins/base_path.ts";
+import jsx_preact from "lume/plugins/jsx_preact.ts";
+import windi_css from "lume/plugins/windi_css.ts";
+import metas from "lume/plugins/metas.ts";
+
+const site = lume({
+  src: "./src",
+  server: {
+    open: true,
+  },
+});
+
+site.use(base_path());
+site.use(metas());
+site.use(jsx_preact());
+site.use(windi_css());
+site.copy("static", ".");
+site.copy([".jpg", ".gif", ".png"]);
+
+export default site;
---
+++ src/posts/my-first-post.md
@@ -0,0 +1,14 @@
+---
+metas:
+  title: Hello world
+  description: My first post
+  image: /hello-world.png
+---
+
+## fdgs
+
+hisfgsj
+
+dghjdgjhdgj
+
+dhgjdgjdjd
